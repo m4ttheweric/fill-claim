8f21d21350d5ad24b0d66a9a467fbe60cb479604
Matt Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2021-05-30 15:27:46 -0700

    Code cleanup

--- src/inject.ts
+++ src/inject.ts
@@ -24,14 +24,12 @@ $(function () {
                window.location.href
             ),
          inject: () => {
-            if (/\?editAndMagic=y/i.test(window.location.href)) {
-               location.href = document.URL.replace(
-                  '?editAndMagic=y',
-                  '/edit?autoMagicFill=y'
-               );
-            } else {
-               location.href = document.URL.replace('?editMe=y', '/edit');
-            }
+            location.href = /\?editAndMagic=y/i.test(window.location.href)
+               ? document.URL.replace(
+                    '?editAndMagic=y',
+                    '/edit?autoMagicFill=y'
+                 )
+               : document.URL.replace('?editMe=y', '/edit');
          },
          injected: false
       },
@@ -41,37 +39,37 @@ $(function () {
          inject: () => {
             const magicFillButtonHtml = `<a class="primary button" href="javascript:void(0)" style="float: right;margin-bottom: 18px;background-color: #8a2be2;">Magic Fill All Prepared Claims!</a>`;
             const openButtonHtml = `<a class="primary button" href="javascript:void(0)" style="float: right;margin-left: 8px;margin-bottom: 18px;background-color: #8a2be2;">Open All Prepared Claims!</a>`;
+            const preparedClaims = () =>
+               $('table.insurance-claims-list tr').filter(function (this: any) {
+                  return (
+                     $(this).find('span.badge:contains("Prepared")').length > 0
+                  );
+               });
 
             function launchClaim(launchStyle: 'magic' | 'open') {
-               const magic = '?editAndMagic=y';
-               const justOpen = '?editMe=y';
-               const queryString = launchStyle === 'magic' ? magic : justOpen;
-
-               $('table.insurance-claims-list tr')
-                  .filter(function (this: any) {
-                     return (
-                        $(this).find('span.badge:contains("Prepared")').length >
-                        0
-                     );
-                  })
-                  .each(function (this: any) {
-                     console.log(
-                        'opening: ',
-                        $(this).data('url') + queryString
-                     );
+               const queryString =
+                  launchStyle === 'magic' ? '?editAndMagic=y' : '?editMe=y';
+               if (preparedClaims().length === 0) {
+                  alert(
+                     'There are currently no claims in the "Prepared" state.'
+                  );
+               } else {
+                  preparedClaims().each(function (this: any) {
                      window.open($(this).data('url') + queryString, '_blank');
                   });
+               }
             }
             function addButton() {
-               const magicBtn = $(magicFillButtonHtml).on('click', () => {
-                  launchClaim('magic');
-               });
-               const openBtn = $(openButtonHtml).on('click', () => {
-                  launchClaim('open');
-               });
-
-               $('#claims').prepend(magicBtn);
-               $('#claims').prepend(openBtn);
+               $(magicFillButtonHtml)
+                  .on('click', () => {
+                     launchClaim('magic');
+                  })
+                  .prependTo('#claims');
+               $(openButtonHtml)
+                  .on('click', () => {
+                     launchClaim('open');
+                  })
+                  .prependTo('#claims');
             }
 
             addButton();
@@ -82,14 +80,14 @@ $(function () {
 
    function testInjections() {
       Object.entries(injections).forEach(([prop, injection]) => {
-         if (injection.urlTest() && injection.injected === false) {
+         if (injection.urlTest() && !injection.injected) {
             injection.inject();
             injection.injected = true;
          }
       });
    }
 
-   testInjections();
-
    window.addEventListener('hashchange', testInjections, false);
+
+   testInjections();
 });
